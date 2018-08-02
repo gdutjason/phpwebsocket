11e0dabfaf8972b5c69917bc26c7653827e79910
gdutjason <754448947@qq.com>
2018-08-02 14:48:23 +0800

    优化各种提示

--- server_swoole.php
+++ server_swoole.php
@@ -11,12 +11,12 @@
     $ws->redis = $redis;
     
     //设置 redis 字符串数据
-    $redis->set("tutorial-name", "Redis tutorial");
+    //$redis->set("tutorial-name", "Redis tutorial");
     // 获取存储的数据并输出
-    echo "Stored string in redis:: " . $redis->get("tutorial-name");
+    //echo "Stored string in redis:: " . $redis->get("tutorial-name");
 
 
-	$ws->nickNames = array(
+	$nickNames = array(
 		"开心的回转寿司",
 		"轻松的大西瓜",
 		"有点伤感的茄子",
@@ -24,32 +24,61 @@
 		"略带心事的沙田柚",
 		"气愤难平的肉卷",
 		"想要洗澡的感冒灵颗粒",
-
+		"贪睡的栀子花",
+		"林枫",
+		"克行",
+		"妒天",
+		"土豆味的马铃薯",
+		"番茄味的西红柿",
+		"肚子饿了的汉堡包",
 	);
+
+	$redis->set("nickNames",json_encode($nickNames));
 
 
 	//监听WebSocket连接打开事件
 	$ws->on('open', function ($ws, $request) {
-	    print_r($request->server);
-	    $key = array_rand($ws->nickNames,1);
-	    $nick = $ws->nickNames[$key];
-	    unset($ws->nickNames[$key]);
-	    $ws->redis->set($request->fd, $nick);
+	    //print_r($request->server);
+		$jsonData = $ws->redis->get("nickNames");
+		$nickNames = json_decode($jsonData,true);
+		print_r($nickNames);
 
-	    $ws->push($request->fd, "你获取的昵称是：{$nick}\n");
+
+	    $key = array_rand($nickNames,1);
+	    $nick = $nickNames[$key];
+	    unset($nickNames[$key]);
+	    //print_r($nickNames);
+	    $ws->redis->set("nickNames",json_encode($nickNames));
+	    $ws->redis->set($request->fd, $nick);
+	    //给本人的提示
+	    $ws->push($request->fd, "你获取的昵称是：{$nick}\n当前本聊天室人数为:".count($ws->connections)."\n");
+	    //给其他人的提示
+	    sendToAll("{$nick}加入了聊天室，当前本聊天室人数为:".count($ws->connections)."\n",$request->fd);
+
 	});
 
 	//监听WebSocket消息事件
 	$ws->on('message', function ($ws, $frame) {
 	    $nick = $ws->redis->get($frame->fd);
-	    echo "昵称是:{$nick}\n";
-	    foreach($ws->connections as $fd){
-	    	$ws->push($fd, "{$nick}:{$frame->data}");
-	    }
+	    sendToAll("{$nick}:{$frame->data}");
 	});
+
+	//向全体发送消息,后面的fd为过滤的本人通道
+	function sendToAll($msg,$filterFd=0){
+		global $ws;
+		foreach($ws->connections as $fd){
+			if($fd == $filterFd){
+
+			}else{
+	    		$ws->push($fd, "{$msg}");
+			}
+	    }
+	}
 
 	//监听WebSocket连接关闭事件
 	$ws->on('close', function ($ws, $fd) {
+		$nick = $ws->redis->get($fd);
+	    sendToAll("{$nick}退出了了聊天室，当前本聊天室人数为:".count($ws->connections)."\n");
 	    echo "client-{$fd} is closed\n";
 	});
 
